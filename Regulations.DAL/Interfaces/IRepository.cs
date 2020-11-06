9cb3df61e6c6abf52ee854dca632862e584d1ce1
vaberezin <vaberezin@protonmail.com>
2020-11-06 23:26:00 +0300

    Add IRepository inteface to DAL

---
+++ Regulations.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Regulations.DAL.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAllItems();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
