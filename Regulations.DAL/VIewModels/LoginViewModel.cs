5a0761a8161c661a45ea6d6d4a45050aa38d3af5
vaberezin <vaberezin@protonmail.com>
2020-11-06 23:20:55 +0300

    Добавьте файлы проекта.

---
+++ Regulations.DAL/VIewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regulations.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required (ErrorMessage = "TypeEmail")]
+        public string Email { get; set; }
+        
+        [Required (ErrorMessage = "TypePassword")]
+        [DataType (DataType.Password)]
+        public string Password { get; set; }    
+    }
+}
---
+++ Regulations.DAL/VIewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regulations.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required (ErrorMessage = "TypeEmail")]
+        public string Email { get; set; }
+        [Required (ErrorMessage = "TypePassword")]
+        [DataType (DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "WrongPassword")]
+        public string ConfirmPassword { get; set; }
+        
+
+    }
+}
